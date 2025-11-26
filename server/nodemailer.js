6fab5c18edf73c8e2ad5af292d69de030288bcdf
BM-ACADEMY <teamkamarweb@gmail.com>
2025-11-26 17:12:43 +0530

    updated

--- server/nodemailer.js
+++ server/nodemailer.js
@@ -4,7 +4,7 @@ const sendMail = async (name, email, phone, university, course, message) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secure: true,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
