d73a3e854c35a73c38db3383be1605025c2da74b
BM-ACADEMY <teamkamarweb@gmail.com>
2025-11-26 16:59:11 +0530

    udpated

--- client/src/Pages/Enquiryform.jsx
+++ client/src/Pages/Enquiryform.jsx
@@ -155,7 +155,7 @@ export function EnquiryFormModal({ isOpen, onOpenChange }) {
     setIsSubmitting(true);
     
     try {
-      const response = await fetch("https://pravishraj-memorial-academy.onrender.com/send-email", {
+      const response = await fetch("https://azhagappa-website.onrender.com/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
